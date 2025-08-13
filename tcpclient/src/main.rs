3a2343616256620b994b859ac49a28e67b895a89
Kode <olorik362@gmail.com>
2025-08-14 00:21:50 +0100

    fix(tcpclient,tcpserver): ensure full writes, proper buffer usage, and graceful client shutdown
    - use write_all to guarantee full message flush
    - slice buffers to avoid reading uninitialized data
    - gracefully terminate the TCP client connection

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,14 +1,23 @@
-use std::net::TcpStream;
+use std::net::{TcpStream,Shutdown};
 use std::io::{Read,Write};
 use std::str;
 
 fn main() {
     let  mut stream = TcpStream::connect("localhost:3000").unwrap();
-    stream.write("Hello world".as_bytes()).unwrap();
+    stream.write_all("Hello world".as_bytes()).unwrap();
 
-    let mut buffer = [0;5];
-    stream.read(&mut buffer).unwrap();
+    let mut buffer = [0;1024];
+  let bytes_read =  stream.read(&mut buffer).unwrap();
+
     println!("Got response from server {:?}", 
-        str::from_utf8(&buffer).unwrap());
+        str::from_utf8(&buffer[..bytes_read]).unwrap());
+    stream.write_all("Closing connection".as_bytes()).unwrap();
+
+let bytes_read   = stream.read(&mut buffer).unwrap();
+    println!("Got response from server {:?}", 
+        str::from_utf8(&buffer[..bytes_read]).unwrap());
+
+    stream.shutdown(Shutdown::Both).unwrap();
+
 
 }
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 use std::io::{Read,Write};
+use std::str;
 
 fn main() {
     let connection_listener=TcpListener::bind("127.0.0.1:3000").unwrap();
@@ -11,7 +12,17 @@ fn main() {
 
         let mut buffer = [0;1024];
 
-        stream.read(&mut buffer).unwrap();
-        stream.write(&mut buffer).unwrap();
+        let bytes_read =stream.read(&mut buffer).unwrap();
+        println!("Client sent {:?}",
+            str::from_utf8(&buffer[..bytes_read] ).unwrap());
+
+        stream.write_all(&mut buffer[..bytes_read]).unwrap();
+
+       let bytes_read= stream.read(&mut buffer).unwrap();
+        println!("Client sent {:?}",
+            str::from_utf8(&buffer[..bytes_read] ).unwrap());
+        stream.write_all("Acknowledged".as_bytes()).unwrap();
+
+
     }
 }
